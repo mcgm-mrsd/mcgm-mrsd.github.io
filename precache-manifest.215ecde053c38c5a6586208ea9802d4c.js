10e6646bd932e81f2b45e2e71bb04548a0cffb68
Algy Taylor <algy.taylor@mft.nhs.uk>
2021-03-18 12:19:38 +0000

    website regenerated

---
+++ precache-manifest.215ecde053c38c5a6586208ea9802d4c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c840afb7d842ece2c93183ca6949fb84",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b5f651547cbafff6eba2",
+    "url": "/static/css/main.8fd5bced.chunk.css"
+  },
+  {
+    "revision": "c69debcfba4149291b96",
+    "url": "/static/js/2.f10303f6.chunk.js"
+  },
+  {
+    "revision": "963be438a25aae1f8d1a6dd3f5fea7df",
+    "url": "/static/js/2.f10303f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b5f651547cbafff6eba2",
+    "url": "/static/js/main.d9c62872.chunk.js"
+  },
+  {
+    "revision": "717be7891cc3d466bc4e",
+    "url": "/static/js/runtime-main.b43b082e.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8f58179cc91fdbd52d8d232c1f62786d.js"
+  "/precache-manifest.215ecde053c38c5a6586208ea9802d4c.js"
 );
 
 self.addEventListener('message', (event) => {
